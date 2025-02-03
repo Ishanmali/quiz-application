3d6fcec88cfde64bfc46894e011f3694eaa839c7
Ishan Malinda <itt1920129@tec.rjt.ac.lk>
2025-02-03 22:06:26 +0530

    update field file

--- src/quiz/application/field.java
+++ src/quiz/application/field.java
@@ -24,3 +24,23 @@ public class Field extends JFrame implements ActionListener {
         image2.setBounds(0, -100, 750, 750);
         add(image2);
         
+        
+                var heading = new JLabel("QuizGenius");
+heading.setBounds(750, 60, 300, 45);
+        heading.setFont(new Font("ALGERIAN", Font.BOLD, 47));
+        heading.setForeground(Color.black);
+        heading.setBackground(Color.blue);
+        add(heading);
+        
+        JLabel name2 = new JLabel("CHOOSE ANY TYPE OF QUESTIONS");
+        name2.setBounds(730, 150, 300, 20);
+        name2.setFont(new Font("ALGERIAN", Font.BOLD, 22));
+        name2.setForeground(Color. green);
+        add(name2);
+        
+        f1 = new JButton("GENERAL");
+        f1.setBounds(735, 250, 250, 50);
+        f1.setBackground(Color.gray);
+        f1.setForeground(Color.WHITE);
+        f1.addActionListener(this);
+        add(f1);
