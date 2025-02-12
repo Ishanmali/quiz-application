8eb4e344e3fc108ab520bb048b84a064134573de
Ishan malinda <153257473+Ishanmali@users.noreply.github.com>
2025-02-12 14:22:10 +0530

    quizset2

---
+++ src/quiz/application/quiz2.java
@@ -0,0 +1,22 @@
+package quiz.application;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+public class Quiz2 extends JFrame implements ActionListener {
+    
+    String questions[][] = new String[10][5];
+    String answers[][] = new String[10][2];
+    String useranswers[][] = new String[10][1];
+    JLabel qno, question;
+    JRadioButton opt1, opt2, opt3, opt4;
+    ButtonGroup groupoptions;
+    JButton next, submit, lifeline;
+    
+    public static int timer = 15;
+    public static int ans_given = 0;
+    public static int count = 0;
+    public static int score = 0;
+    
+    public String name;
