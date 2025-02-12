c00e1460c2b6246a3c82d634ca486788ff85bf5c
Ishan malinda <153257473+Ishanmali@users.noreply.github.com>
2025-02-12 14:24:46 +0530

    update quiz

--- src/quiz/application/quiz2.java
+++ src/quiz/application/quiz2.java
@@ -20,3 +20,26 @@ public class Quiz2 extends JFrame implements ActionListener {
     public static int score = 0;
     
     public String name;
+
+     
+    Quiz2(String name) {
+        this.name = name;
+        setBounds(50, 0, 1500,700 );
+        getContentPane().setBackground(Color.WHITE);
+        setLayout(null);
+        
+        ImageIcon i1 = new ImageIcon(ClassLoader.getSystemResource("icons/login.jpeg"));
+        JLabel image = new JLabel(i1);
+        image.setBounds(0, 0, 1500, 392);
+        add(image);
+        
+        qno = new JLabel();
+        qno.setBounds(100, 450, 50, 30);
+        qno.setFont(new Font("Tahoma", Font.PLAIN, 24));
+        add(qno);
+        
+        question = new JLabel();
+        question.setBounds(150, 450, 900, 30);
+        question.setFont(new Font("Tahoma", Font.PLAIN, 24));
+        add(question);
+        
