01b7eaaffc57187eac7b229bbafa64776eab97fd
Ishan malinda <153257473+Ishanmali@users.noreply.github.com>
2025-02-16 22:37:31 +0530

    update rules.js

---
+++ src/quiz/application/rules.java
@@ -0,0 +1,14 @@
+package quiz.application;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class Rules extends JFrame implements ActionListener{
+
+    String name;
+    JButton start, back;
+    
