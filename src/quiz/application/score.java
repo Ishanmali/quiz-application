2b43897cfb65b7d683bee2776c45693f4b9b8a0e
Ishan malinda <153257473+Ishanmali@users.noreply.github.com>
2025-02-18 23:58:41 +0530

    updae score

---
+++ src/quiz/application/score.java
@@ -0,0 +1,19 @@
+package quiz.application;
+
+import java.awt.*;
+import javax.swing.*;
+import java.awt.event.*;
+
+public class Score extends JFrame implements ActionListener {
+
+    Score(String name, int score) {
+        setBounds(400, 150, 750, 550);
+        getContentPane().setBackground(Color.YELLOW);
+        setLayout(null);
+        
+        ImageIcon i1 = new ImageIcon(ClassLoader.getSystemResource("icons/score.jpeg"));
+        Image i2 = i1.getImage().getScaledInstance(300, 250, Image.SCALE_DEFAULT);
+        ImageIcon i3 = new ImageIcon(i2);
+        JLabel image = new JLabel(i3);
+        image.setBounds(0, 200, 300, 250);
+        add(image);
