2c0424cc3f32697f99718c809143417e99bb736a
Ishan malinda <153257473+Ishanmali@users.noreply.github.com>
2025-02-18 23:59:23 +0530

    Update score.java

--- src/quiz/application/score.java
+++ src/quiz/application/score.java
@@ -17,3 +17,14 @@ public class Score extends JFrame implements ActionListener {
         JLabel image = new JLabel(i3);
         image.setBounds(0, 200, 300, 250);
         add(image);
+
+        JLabel heading = new JLabel("Thankyou " + name + " for playing QUIZ SHOW developed by NK  ");
+        heading.setBounds(45, 30, 700, 30);
+        heading.setFont(new Font("TIMES NEW ROMAN", Font.BOLD,20));
+        heading.setForeground(Color.red);
+        add(heading);
+        
+        JLabel lblscore = new JLabel("Your score is " + score);
+        lblscore.setBounds(350, 200, 300, 30);
+        lblscore.setFont(new Font("Tahoma", Font.BOLD, 26));
+        add(lblscore);
